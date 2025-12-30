f03d8b621ad8157d54551b85adf4b793d915a6f4
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-12-30 06:10:08 -0300

    feat: adiciona comando de cancelar

--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@
 #define EXIT		'E'
 #define ENCRIPT		'C'
 
+void ari_commands(void)
+{
+	printf("Opções disponíveis\n");
+	printf("C	Criptografar mensagens\n");
+	printf("Comandos disponíveis\t\n");
+	printf("\"_cancelar\"	Encerra a seção (É necessário estar em seção)\n");
+	printf("___________________________________________\n");
+}
+
 int main(void)
 {
 	setlocale(LC_ALL, "ptb");
@@ -34,12 +43,18 @@ int main(void)
 				printf("ARI\\criptografia>");
 				char message[101];
 				scanf(" %100[^\n]", message);
+
+				if (strcmp(message, "_cancelar") == 0)
+					break;
 								
 				printf("Informe o nome do arquivo\n");
 				
 				printf("ARI\\criptografia>");
 				char file_name[32];
 				scanf(" %31[^\n]", file_name);
+
+				if (strcmp(file_name, "_cancelar") == 0)
+					break;				
 							
 		    	if (encrypt(file_name, message))
 					printf("Mensagem criptografada\n");
