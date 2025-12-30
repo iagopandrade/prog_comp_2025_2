4c0a4086317dee4dddac6060f89057478a15c580
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-12-30 06:30:10 -0300

    feat: adiciona novas opções ao menu

--- src/main.c
+++ src/main.c
@@ -5,15 +5,23 @@
 #include <ctype.h>
 
 #include "aricd.h"
-#include "ario.h"
 
-#define EXIT		'E'
 #define ENCRIPT		'C'
+#define DECRYPT		'D'
+#define VIEWER	    'V'
+#define EXIT		'E'
+#define LIMPAR		'L'
+#define AJUDA		'A'
 
 void ari_commands(void)
 {
 	printf("Opções disponíveis\n");
 	printf("C	Criptografar mensagens\n");
+	printf("D	Descriptografar mensagens\n");
+	printf("V   	Visualizar mensagens\n");
+	printf("E   	Encerrar programa\n");
+	printf("L	Limpa o terminal\n");
+	printf("A	Exibe os comandos do programa\n");
 	printf("Comandos disponíveis\t\n");
 	printf("\"_cancelar\"	Encerra a seção (É necessário estar em seção)\n");
 	printf("___________________________________________\n");
@@ -22,7 +30,9 @@ void ari_commands(void)
 int main(void)
 {
 	setlocale(LC_ALL, "ptb");
-	printf(" Seja bem vindo ao melhor encriptador de mensagens.\n\n");
+	printf("ARI - O melhor programa de  criptografia.\n");
+
+	ari_commands();
 
 	while(1)
 	{
@@ -35,6 +45,8 @@ int main(void)
 		switch(op)
 		{
 			case EXIT: exit(0);
+			case LIMPAR: system("cls"); break;
+			case AJUDA: ari_commands(); break;
 			
 		    case ENCRIPT:
 		    {
@@ -61,6 +73,48 @@ int main(void)
 				break;
 			}
 
+			case DECRYPT:
+			{
+    			printf("Informe o nome de um arquivo\n");  			
+    			
+    			printf("ARI\\descriptografar>");
+				char in[32];
+    			scanf(" %31[^\n]", in);
+    			
+    			if (strcmp(in, "_cancelar") == 0)
+					break;
+				
+				if (decrypt(in))
+					printf("%s foi descriptografado\n", in);
+				break;
+			}
+
+			case VIEWER:
+			{
+				printf("Informe o nome do arquivo\n");
+				
+				printf("ARI\\visualizar>");
+				char in[32];
+				scanf(" %31[^\n]", in);
+				
+				if (strcmp(in, "_cancelar") == 0)
+					break;
+				
+				FILE *file = fopen(in, "r");
+				if (file == NULL)
+				{
+			        perror("");
+			        break;
+			    }
+				
+				char message[101];	
+				fscanf(file, "%100[^\n]", message);
+				printf("%s: %s\n", in, message);
+				
+				fclose(file);
+				break;
+			}
+		
 			default: printf("'%c' opção inválida\n", op);
 		}
 	}
